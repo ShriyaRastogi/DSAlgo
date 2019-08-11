67165745fe26db6da5f38724b1506c540c06e08f
16je002197 <banipreet.raheja@gmail.com>
2019-08-12 00:08:29 +0530

    day of date updated

--- Algorithms/DayOfDate.cpp
+++ Algorithms/DayOfDate.cpp
@@ -16,6 +16,11 @@ ll gcd(ll a,ll b) { if (b == 0) return a; return gcd(b, a % b);  }
 ll mod(ll x) { return x%MOD; }
 ll power(ll x,ll y, ll p) { ll res = 1; x = x % p; while (y > 0) { if (y & 1) res = (res*x) % p; y = y>>1; x = (x*x) % p;   } return res; } 
 //class compare { public: bool operator() (ll a,ll b) { return a>b;};
+ll getLeap(ll y, ll m)
+{
+	if(m>2) return y/4 - y/100 + y/400;
+	return (y-1)/4 - (y-1)/100 + (y-1)/400;
+}
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -24,27 +29,36 @@ int main()
 	cin>>t;
 	while(t--)
 	{
-		ll date,month,year;
-		cin>>date>>month>>year;
-		ll odd = 0;
-		year = year%400;
-		ll hund = year/100;
-		if(hund==1) odd+=5;
-		else if(hund==2) odd+=3;
-		else if(hund==3) odd+=1;
-		year = year%100;
-		ll y = year-1;
-		odd += (y/4)*2 + (y-(y/4))*1; // leap year has 2 odd days and normal year has 1
-		vector<ll> mon = {31,28,31,30,31,30,31,31,30,31,30,31};
-		// if leap year, remove add 1
-		if((year%4==0 && year%100 != 0) || (year%400==0)) mon[1]++;
+		ll d1,m1,y1;
+		ll d2,m2,y2;
+		d2 = 12;
+		m2 = 8;
+		y2 = 2019;
+		cin>>d1>>m1>>y1;
+		vector<ll> arr = {31,28,31,30,31,30,31,31,30,31,30,31};
 		
-		for(ll i=0;i<12;++i) mon[i] = mon[i]%7;
-		for(ll i=0;i<month-1;++i) odd+=mon[i];
-		odd+=date;
+		ll count1 = y1*365;
+		for(ll i=0;i<m1-1;++i) count1+= arr[i];
+		count1+= d1;
+		count1+= getLeap(y1,m1); 
 		
-		odd = odd %7;
-		vector<string> ans = {"Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-		cout<<ans[odd]<<endl;
+			
+		ll count2 = y2*365;
+		for(ll i=0;i<m2-1;++i) count2+= arr[i];
+		count2+= d2;
+		count2+= getLeap(y2,m2); 
+		vector<string> arr2 = {"Sunday", "Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday"};
+		if(count2 > count1)
+		{
+			ll temp = (count2-count1)%7;
+			temp = (7-temp)%7;
+			cout<<arr2[(temp+1)%7]<<endl;
+		}
+		else
+		{
+			ll temp = (count1-count2)%7;
+			cout<<arr2[(temp+1)%7]<<endl;
+		}
+		
 	}
 }
